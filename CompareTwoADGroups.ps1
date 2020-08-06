5e0ce3cae6533dd45ef9e3778b4320f44d948588
Will Burgess <burgeswe@hotmail.com>
2020-08-06 17:30:47 -0500

    Add files via upload

---
+++ CompareTwoADGroups.ps1
@@ -0,0 +1,6 @@
+﻿$FirstGroup = Get-ADGroupMember 'first group'
+$SecondGroup = Get-ADGroupMember 'second group'
+#$csvPath = 'path for exportable csv if desired'
+
+Compare-Object $FirstGroup $SecondGroup -Property 'SamAccountName' -IncludeEqual
+Compare-Object $FirstGroup $SecondGroup -Property 'SamAccountName' -IncludeEqual | Export-Csv $csvPath -NoTypeInformation
