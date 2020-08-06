a147efa581e9af55e69fb523b6fd3f6b05bacea2
Will Burgess <burgeswe@hotmail.com>
2020-08-06 17:31:19 -0500

    Update CompareTwoADGroups.ps1

--- CompareTwoADGroups.ps1
+++ CompareTwoADGroups.ps1
@@ -3,4 +3,4 @@ $SecondGroup = Get-ADGroupMember 'second group'
 #$csvPath = 'path for exportable csv if desired'
 
 Compare-Object $FirstGroup $SecondGroup -Property 'SamAccountName' -IncludeEqual
-Compare-Object $FirstGroup $SecondGroup -Property 'SamAccountName' -IncludeEqual | Export-Csv $csvPath -NoTypeInformation
+#Compare-Object $FirstGroup $SecondGroup -Property 'SamAccountName' -IncludeEqual | Export-Csv $csvPath -NoTypeInformation
