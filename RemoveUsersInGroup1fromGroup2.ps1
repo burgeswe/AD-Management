c9cddfa9e8dad7e035aa98531cf62ecdeceffef4
Will Burgess <burgeswe@hotmail.com>
2020-08-06 17:18:47 -0500

    Add files via upload

---
+++ RemoveUsersInGroup1fromGroup2.ps1
@@ -0,0 +1,8 @@
+﻿$group1 = Get-ADGroupMember -Identity 'groupname1' -Recursive | %{Get-ADUser $_.samAccountName | select samAccountName}
+$group2 = Get-ADgroup -Identity 'groupname2'
+
+Foreach ($user in $group1)
+
+{
+Remove-adgroupmember -identity $group2 -members $user.SamAccountName -Confirm:$false
+}
